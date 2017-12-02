7261daa0e66b1d96d668dfa8bee7a14ace4f1a75
Cole Chronowski <Cole.Chronowski@ou.edu>
2017-12-02 15:00:25 -0600

    training set

---
+++ training_set.py
@@ -0,0 +1,11 @@
+[
+	('Hello', 'greeting'),
+	('How are you?', 'greeting'),
+	('Hi', 'greeting'),
+	('Hi everyone', 'greeting'),
+	('Dumb', 'insult'),
+	('Hate', 'insult'),
+	('Idiot', 'insult'),
+	('Moron', 'insult'),
+	('Stupid', 'insult')
+]
